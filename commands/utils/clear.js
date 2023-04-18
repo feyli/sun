d43c71b85d625698341b00a16b43ba8e4475f3e8
Feyli <mathiscom64@gmail.com>
2023-04-18 02:45:48 +0200

    added clear command
    Took 53 minutes

--- commands/utils/clear.js
+++ commands/utils/clear.js
@@ -1,15 +1,24 @@
 module.exports = {
   command_data: {
     name: 'clear',
-    description: 'Displays a list of the members of a role.',
+    description: 'Deletes the specified amount of messages in the active channel.',
     type: 1,
     dm_permission: false,
+    default_member_permissions: 8192,
     options: [
       {
         name: 'amount',
-        description: 'The role to get a list of members for.',
+        description: 'The amount of messages to delete.',
         type: 4,
+        min_value: 1,
+        max_value: 100,
         required: true,
+      },
+      {
+        name: 'user',
+        description: 'The user to delete messages from.',
+        type: 6,
+        required: false,
       },
     ],
   },
@@ -18,13 +27,36 @@ module.exports = {
   perms: null,
   owner_only: false,
   run: async (client, interaction) => {
-    await interaction.deferReply({ ephemeral: true })
-    let role = interaction.options.get('role').role
-    let members = role.members.map((member) => member).join(', ')
-    let embed = {
-      title: `Members of \`${role.name}\``,
-      description: members,
+    const amount = interaction.options.getInteger('amount');
+    const user = interaction.options.getUser('user');
+
+    await interaction.deferReply({ ephemeral: true });
+
+    const messages = await interaction.channel.messages.fetch();
+
+    if (user) {
+      let i = 0;
+      const filtered = [];
+
+      try {
+        (await messages).filter((message) => {
+          if (message.author.id === user.id && amount > i) {
+            filtered.push(message);
+            i++;
+          }
+        });
+      } catch (e) {
+        console.error(e);
+      }
+      await interaction.channel.bulkDelete(filtered, true);
+      await interaction.editReply({ embeds: [{ description: 'Deleted ' + amount + ' messages from ' + user.toString() + '.' }] })
+    } else {
+      await interaction.channel.bulkDelete(amount, true);
+      await interaction.editReply({ embeds: [{ description: 'Deleted ' + amount + ' messages.' }] })
     }
-    await interaction.editReply({ embeds: [embed] })
+
+    setTimeout(() => {
+      interaction.deleteReply();
+    }, 5000)
   },
-}
+};
