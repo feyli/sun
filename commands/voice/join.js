b830619683de9022c101f419458f7eb778886865
Feyli <mathiscom64@gmail.com>
2023-05-02 14:33:59 +0200

    fixed bugs
    Took 10 minutes

--- commands/voice/join.js
+++ commands/voice/join.js
@@ -19,13 +19,13 @@ module.exports = {
     category: 'Voice',
     cooldown: 5000,
     run: async (client, interaction) => {
-        const voiceChannel = interaction.options.getChannel('channel') || interaction.member.voice.channel;
+        const voiceChannel = interaction.options.getChannel('channel') || interaction.member.voice?.channel;
         // check if bot is already in voiceChannel (the same as voiceChannel)
         let connection = getVoiceConnection(interaction.guild.id);
-        if (connection?.joinConfig?.channelId === voiceChannel.id) return interaction.reply(`Already in ${voiceChannel}.`);
-
         if (!voiceChannel) return interaction.reply('You must be in a voice channel or specify one to join.');
-        if (!interaction.member.voice.channel.joinable) return interaction.reply('I do not have permission to join that voice channel.');
+
+        if (connection?.joinConfig?.channelId === voiceChannel.id) return interaction.reply(`Already in ${voiceChannel}.`);
+        if (!voiceChannel.joinable) return interaction.reply('I do not have permission to join that voice channel.');
         await joinVoiceChannel({
             channelId: voiceChannel.id,
             guildId: interaction.guild.id,
