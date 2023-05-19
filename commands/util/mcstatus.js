8a9782f10f720a1650d7d1a10d4f08653d8cb977
Feyli <mathiscom64@gmail.com>
2023-05-19 23:54:53 +0200

    added motd preview
    Took 25 minutes

--- commands/util/mcstatus.js
+++ commands/util/mcstatus.js
@@ -3,6 +3,7 @@
 const { AttachmentBuilder, EmbedBuilder } = require("discord.js");
 const mslp = require('minecraft-status').MinecraftServerListPing;
 const net = require('net');
+const Jimp = require('jimp');
 
 module.exports = {
     command_data: {
@@ -15,7 +16,7 @@ module.exports = {
     category: 'Utility',
     cooldown: 5000,
     run: async (client, interaction) => {
-        await interaction.deferReply()
+        await interaction.deferReply();
         const db = client.db;
 
         const { address, port } = await db.query('SELECT address, port FROM mcstatus WHERE guild_id = ?', [interaction.guild.id]).then((res) => res[0]);
@@ -24,13 +25,13 @@ module.exports = {
 
         // await interaction.deferReply({ ephemeral: false });
 
-        const res = await mslp.ping(4, address, port, 10000).catch(() => {
+        const res = await mslp.ping(4, address, port, 6000).catch(() => {
             return { players: { max: 0 } };
         });
 
         let latency;
 
-        if (res.players) {
+        if (res.players && res.players.max !== 0) {
             const pingPromise = new Promise((resolve, reject) => {
                 const sock = new net.Socket();
                 sock.setTimeout(10000);
@@ -48,15 +49,15 @@ module.exports = {
                 }).connect(port, address);
             });
 
-            await pingPromise
+            await pingPromise.catch();
         }
 
 
         let buffer;
-        let attachment;
+        let favicon;
         if (res.favicon) {
             buffer = Buffer.from(res.favicon.split(',')[1], 'base64');
-            attachment = new AttachmentBuilder(buffer, { name: 'favicon.png' });
+            favicon = new AttachmentBuilder(buffer, { name: 'favicon.png' });
         }
 
         const embed = new EmbedBuilder()
@@ -83,7 +84,7 @@ module.exports = {
                 iconURL: interaction.user.avatarURL({ dynamic: true })
             })
             .setColor(res.players.max !== 0 ? 0x65CDB6 : 0xF04251)
-            .setThumbnail(attachment ? 'attachment://favicon.png' : 'https://media.minecraftforum.net/attachments/300/619/636977108000120237.png');
+            .setThumbnail(favicon ? 'attachment://favicon.png' : 'https://media.minecraftforum.net/attachments/300/619/636977108000120237.png');
 
         if (res.players.max !== 0) {
             embed.addFields(
@@ -107,6 +108,15 @@ module.exports = {
         }
 
         // only include attachment if not null
-        await interaction.editReply({ embeds: [embed], files: attachment ? [attachment] : [] });
+        await interaction.editReply({ embeds: [embed], files: favicon ? [favicon] : [] });
+
+        if (res.players.max !== 0) {
+            const motdImage = `https://sr-api.sfirew.com/server/${address}:${port}/banner/motd.png?hl=en&v=67DvWb4tVJ`;
+            const image = await Jimp.read(motdImage);
+            image.crop(230, 66, 1580, 150);
+            const motdBanner = new AttachmentBuilder(await image.getBufferAsync(Jimp.MIME_PNG), { name: 'motd.png' });
+            embed.setImage('attachment://motd.png');
+            await interaction.editReply({ embeds: [embed], files: favicon ? [favicon, motdBanner] : [motdBanner] });
+        }
     }
 };
