bcd61e7621b6d4d1054047677e83c7bd8ee7c133
Feyli <mathiscom64@gmail.com>
2023-06-05 19:21:11 +0200

    changed db ref
    Took 57 seconds

--- config/main.js
+++ config/main.js
@@ -1,45 +1,45 @@
-const { ClientIntents, ClientPartials } = require('discord.js-v14-helper')
+const { ClientIntents, ClientPartials } = require('discord.js-v14-helper');
 
 module.exports = {
-  // Client configuration:
-  client: {
-    constructor: {
-      intents: ClientIntents,
-      partials: ClientPartials,
-      presence: {
-        activities: [
-          {
-            name: `Preparing the presence...`,
-            type: 0,
-          },
-        ],
-        status: 'online',
-      },
+    // Client configuration:
+    client: {
+        constructor: {
+            intents: ClientIntents,
+            partials: ClientPartials,
+            presence: {
+                activities: [
+                    {
+                        name: `Preparing the presence...`,
+                        type: 0,
+                    },
+                ],
+                status: 'online',
+            },
+        },
+        token: process.env.TOKEN,
+        id: '743826135061889028',
     },
-    token: process.env.TOKEN,
-    id: '743826135061889028',
-  },
 
-  // Users:
-  users: {
-    owner: '738852700816670822',
-  },
+    // Users:
+    users: {
+        owner: '738852700816670822',
+    },
 
-  channels: {
-    logging_channel: '1092552794323550258',
-  },
-  databases: [
-      {
-    host: '185.142.53.15',
-    user: 'feyli',
-    password: process.env.DBPASSWORD,
-    database: 'sunbot',
-  },
-    {
-      host: '185.142.53.15',
-      user: 'arcane',
-      password: process.env.ARCANEDBPASSWORD,
-      database: 'arcane_blades',
-    }
-  ]
-}
+    channels: {
+        logging_channel: '1092552794323550258',
+    },
+    databases: [
+        {
+            host: '185.142.53.15',
+            user: 'feyli',
+            password: process.env.DBPASSWORD,
+            database: 'sunbot',
+        },
+        {
+            host: '185.142.53.15',
+            user: 'arcane',
+            password: process.env.ARCANEDBPASSWORD,
+            database: 'arcane_blades',
+        }
+    ]
+};
