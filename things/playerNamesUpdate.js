46b7b684c6f40ca6b9c8e8309550a819aa5a5217
Feyli <mathiscom64@gmail.com>
2024-04-05 09:37:47 +0200

    rearranged

--- things/playerNamesUpdate.js
+++ things/playerNamesUpdate.js
@@ -10,9 +10,9 @@ module.exports = async() => {
     for (const player of players) {
         const res = await fetch("https://sessionserver.mojang.com/session/minecraft/profile/" + player.player_uuid).then(res => res.json()).catch(() => null);
         if (!res) continue;
-        console.log("Player: " + player.player_uuid + " Name: " + res.name)
         const username = res.name;
         playerArray.push([player.player_uuid, username, username]);
+        console.log("Player: " + player.player_uuid + " Name: " + username)
     }
     console.log(playerArray);
     await db.batch('INSERT INTO players (player_uuid, player_name) VALUES (?, ?) ON DUPLICATE KEY UPDATE player_name=?', playerArray);
