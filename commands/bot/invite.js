6a8cb29de68d4a554b8adbf9f9ac58c7c8b3407d
Feyli <mathiscom64@gmail.com>
2024-06-06 11:57:04 +0200

    formatted text

--- commands/bot/invite.js
+++ commands/bot/invite.js
@@ -9,8 +9,9 @@ module.exports = {
     run: async (client, interaction) => {
         await interaction.reply({
             content: "Invite me to your **server** or add me to your **account** by clicking [here](https://discord.com/oauth2/authorize?client_id=743826135061889028) or by clicking \"Add App\" under my profile!\n" +
-            "After adding me to your account, you'll be able to use me in any server or conversation you're in! Don't worry, I only have access to the message or profile you interact with me in. [Learn more](https://canary.discord.com/oauth2/authorize?client_id=743826135061889028).",
+                "After adding me to your account, you'll be able to use me in __any__ server or conversation you're in!\n" +
+                "**Don't worry**, I only have access to the **message** or **profile** you interact with me in. [Learn more](https://canary.discord.com/oauth2/authorize?client_id=743826135061889028).",
             ephemeral: false,
         });
     },
-}
+};
