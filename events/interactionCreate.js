f2a82a9c249d9d1ffc440f925536a9a6d868391c
Feyli <mathiscom64@gmail.com>
2024-06-06 13:43:16 +0200

    removed logs and fixed cd once again

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,7 +3,6 @@ const config = require('../config/main');
 
 client.on('interactionCreate', async (interaction) => {
     const identifier = interaction.commandName || interaction.customId;
-    console.log("Identifier: " + identifier);
 
     // command checking
     if (!client.commands.has(identifier)) return interaction.reply(
@@ -16,7 +15,6 @@ client.on('interactionCreate', async (interaction) => {
 
     // cooldown checking
     if (client.commands.get(identifier).cooldown !== 0 && interaction.user.id.toString() !== config.users.owner) {
-        console.log("Command has a cooldown and user is not the owner.");
         if (client.cooldowns.get(identifier).get(interaction.user.id)) {
             return interaction.reply({
                 content: 'You are on cooldown! This command has a cooldown of `' +
